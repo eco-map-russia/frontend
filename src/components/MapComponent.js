c46dac767d9a3a59d413edca5fc5f7bb5f2d6a8c
JolyPatt <achzhanov@gmail.com>
2025-07-28 16:01:10 +0300

    Switched to Yandex Map

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,19 +1,13 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { YMaps, Map } from '@pbe/react-yandex-maps';
 
-function MapComponent() {
-  const position = [55.7558, 37.6176]; // Координаты центра карты (Москва)
-
+function MapComponent(props) {
   return (
-    <MapContainer center={position} zoom={10} style={{ height: '400px', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>Москва</Popup>
-      </Marker>
-    </MapContainer>
+    <YMaps>
+      <div>
+        My awesome application with maps!
+        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} width="100%" height="400px" />
+      </div>
+    </YMaps>
   );
 }
 
