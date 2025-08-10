c6d3d2309df2ae03d4205b7e60236a9dc5962b07
JolyPatt <achzhanov@gmail.com>
2025-08-10 17:11:30 +0300

    Working on UI V-2

--- src/components/UI/SideBar.js
+++ src/components/UI/SideBar.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import tBankLogo from '../../assets/images/T-bank-sidebar-logo.svg';
 import analyticsIcon from '../../assets/images/sidebarIcons/Analytics_1.svg';
 import inboxIcon from '../../assets/images/sidebarIcons/Inbox_2.svg';
@@ -7,46 +9,69 @@ import helpCircleIcon from '../../assets/images/sidebarIcons/Help-Circle_5.svg';
 import logOutIcon from '../../assets/images/sidebarIcons/Log-out_6.svg';
 
 function Sidebar() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className="sidebar sidebar--open">
-      {/* Верхний блок: логотип и кнопка-стрелка */}
-      <div className="sidebar__top">
-        <div className="sidebar__logo">
-          <img src={tBankLogo} alt="T Банк" />
+    <div className={`sidebar-container ${expanded ? 'is-expanded' : ''}`}>
+      <aside className={`sidebar ${expanded ? 'sidebar--expanded' : 'sidebar--collapsed'}`}>
+        {/* Верхний блок: логотип и кнопка-стрелка */}
+        <div className="sidebar__top">
+          <div className="sidebar__logo">
+            <img src={tBankLogo} alt="T Банк" />
+          </div>
+          {/* Имя пользователя (видно только в развернутом) */}
+          <div className="sidebar__user">
+            <div className="sidebar__user-name">Имя Фамилия</div>
+          </div>
+
+          <button
+            className="sidebar__toggle"
+            onClick={() => setExpanded(!expanded)}
+            aria-label={expanded ? 'Свернуть меню' : 'Развернуть меню'}
+          >
+            <div className="sidebar__toggle__arrow" />
+          </button>
         </div>
-        <button className="sidebar__toggle">
-          <div className="sidebar__toggle__arrow" />
-        </button>
-      </div>
 
-      {/* Основные кнопки-иконки */}
-      <nav className="sidebar__menu">
-        <button className="sidebar__btn">
-          <img src={analyticsIcon} alt="T Банк" />
-        </button>
-        <button className="sidebar__btn">
-          <img src={inboxIcon} alt="T Банк" />
-        </button>
-        <button className="sidebar__btn">
-          <img src={calendarIcon} alt="T Банк" />
-        </button>
-        <button className="sidebar__btn">
-          <img src={reportsIcon} alt="T Банк" />
-        </button>
-      </nav>
+        <div className="sidebar__divider" />
 
-      {/* Пустое пространство для выравнивания нижних иконок */}
-      <div className="sidebar__spacer" style={{ flexGrow: 1 }} />
+        {/* Основные кнопки-иконки */}
+        <nav className="sidebar__menu">
+          <button className="sidebar__btn">
+            <img className="sidebar__icon" src={analyticsIcon} alt="T Банк" />
+            <span className="sidebar__label">Личный кабинет</span>
+          </button>
+          <button className="sidebar__btn">
+            <img className="sidebar__icon" src={inboxIcon} alt="T Банк" />
+            <span className="sidebar__label">Избранные регионы</span>
+            <span className="sidebar__caret" aria-hidden="true">
+              ▾
+            </span>
+          </button>
+          <button className="sidebar__btn">
+            <img className="sidebar__icon" src={calendarIcon} alt="T Банк" />
+            <span className="sidebar__label">О нас</span>
+          </button>
+          <button className="sidebar__btn">
+            <img className="sidebar__icon" src={reportsIcon} alt="T Банк" />
+            <span className="sidebar__label">Настройки</span>
+          </button>
+        </nav>
 
-      {/* Нижние иконки */}
-      <div className="sidebar__bottom">
-        <button className="sidebar__btn">
-          <img src={helpCircleIcon} alt="T Банк" />
-        </button>
-        <button className="sidebar__btn sidebar__btn--exit">
-          <img src={logOutIcon} alt="T Банк" />
-        </button>
-      </div>
+        {/* Пустое пространство для выравнивания нижних иконок */}
+        <div className="sidebar__spacer" style={{ flexGrow: 1 }} />
+
+        {/* Нижние иконки */}
+        <div className="sidebar__bottom">
+          <button className="sidebar__btn">
+            <img className="sidebar__icon" src={helpCircleIcon} alt="T Банк" />
+            <span className="sidebar__label">Помощь</span>
+          </button>
+          <button className="sidebar__btn sidebar__btn--exit">
+            <img className="sidebar__icon" src={logOutIcon} alt="T Банк" />
+            <span className="sidebar__label">Выйти</span>
+          </button>
+        </div>
+      </aside>
     </div>
   );
 }
